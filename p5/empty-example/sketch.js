6b271b43b94cfd06212369568ea689ec83847279
lsmanoel <ls.manoel@gmail.com>
2018-07-15 01:04:53 -0300

    2
    w

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -3,7 +3,7 @@ var osc, fft;
 //-----------------------------------------------------------------------------
 var slider_A, slider_D, slider_S, slider_R;
 var slider_attackLevel, slider_releaseLevel;
-var slider_delayTime, slider_delayFeedback, slider_delayFilter;
+var slider_delayTime, slider_delayFeedback, slider_delayFilterCutoff;
 //-----------------------------------------------------------------------------
 //Envelope var:
 var env;
@@ -21,7 +21,7 @@ var releaseTime = 0.5;
 var delay;
 var delayTime = 1;
 var delayFeedback = 1;
-var delayFilter = 1;
+var delayFilterCutoff = 2300;
 
 function setup() 
 {
@@ -33,6 +33,7 @@ function setup()
 	slider_A = createSlider(0, 1,  0.01, 0);
 	slider_A.position(10, 10);
 	slider_A.style('height,', '80px');
+	
 
 	slider_D = createSlider(0, 1, 0.2, 0);
 	slider_D.position(10, 40);
@@ -57,6 +58,14 @@ function setup()
 	slider_delayTime = createSlider(0, 4, 1, 0);
 	slider_delayTime.position(10, 210);
 	slider_delayTime.style('height,', '80px');
+
+	slider_delayFeedback = createSlider(0, 0.99, 0.5, 0);
+	slider_delayFeedback.position(10, 240);
+	slider_delayFeedback.style('height,', '80px');
+
+	slider_delayFilterCutoff = createSlider(0, 2300, 1150, 0);
+	slider_delayFilterCutoff.position(10, 270);
+	slider_delayFilterCutoff.style('height,', '80px');
 
 	//-----------------------------------------------------------------------------
 	osc = new p5.SawOsc(); // set frequency and type
@@ -85,7 +94,6 @@ function setup()
 	//Delay:
 	delay = new p5.Delay();
 	delay.process(osc, .12, .7, 2300);
-	delay.delayTime(1);
 
 	//-----------------------------------------------------------------------------
 	background(0, 255, 0);	
@@ -93,7 +101,7 @@ function setup()
 
 function draw() 
 {
-	background(0);
+	background(100, 100, 100);
 
 	//-----------------------------------------------------------------------------
 	//FFT:
@@ -129,12 +137,18 @@ function draw()
 	//-----------------------------------------------------------------------------
 	// change envelope:	
 	attackLevel = slider_attackLevel.value();
+	text("A level", slider_attackLevel.x * 2 + slider_attackLevel.width, slider_attackLevel.y +10);
 	releaseLevel = slider_releaseLevel.value();
+	text("R level", slider_attackLevel.x * 2 + slider_attackLevel.width, slider_releaseLevel.y +10);
 
 	attackTime = slider_A.value();
+	text("A", slider_A.x * 2 + slider_A.width, slider_A.y + 10);
 	decayTime = slider_D.value();
+	text("D", slider_A.x * 2 + slider_D.width, slider_D.y + 10);
 	susPercent = slider_S.value();
+	text("S", slider_A.x * 2 + slider_S.width, slider_S.y + 10);
 	releaseTime = slider_R.value();
+	text("R", slider_A.x * 2 + slider_R.width, slider_R.y + 10);
 
 	env.setADSR(attackTime, decayTime, susPercent, releaseTime);
 	env.setRange(attackLevel, releaseLevel);
@@ -142,10 +156,15 @@ function draw()
 	//-----------------------------------------------------------------------------
 	// change delay:
 	delayTime = slider_delayTime.value();
-	//delayFeedback = slider_delayFeedback.value();
-	//delayFilter = slider_delayFilter.value();
+	text("Delay Time", slider_delayTime.x * 2 + slider_delayTime.width, slider_delayTime.y + 10);
+	delayFeedback = slider_delayFeedback.value();
+	text("Delay Feedback", slider_delayFeedback.x * 2 + slider_delayFeedback.width, slider_delayFeedback.y + 10);
+	delayFilterCutoff = slider_delayFilterCutoff.value();
+	text("Delay CutOff", slider_delayFilterCutoff.x * 2 + slider_delayFilterCutoff.width, slider_delayFilterCutoff.y + 10);
+
 	delay.delayTime(delayTime);
-	delay.process(osc, .12, .7, 2300);		
+	delay.feedback(delayFeedback);
+	delay.filter(delayFilterCutoff);
 }
 
 function playEnv()
